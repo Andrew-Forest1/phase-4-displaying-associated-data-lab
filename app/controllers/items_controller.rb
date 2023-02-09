2e1784a6dbba8da9617684e8f6fceddb3927c9ef
Andrew <aforest41@yahoo.com>
2023-02-09 15:33:59 -0800

    commit tree baf4fc15a72ef243d2b5c12851b388ae5e7e79c8

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,6 @@
 class ItemsController < ApplicationController
+    def index
+        items = Item.all
+        render json: items, include: :user #only: [:description, :name, :price] include: [user: {:username, :city}], status: :ok
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :items, only: [:index]
-  resources :users, only: [:show]
+  resources :users, only: [:show] #this generates the same as the code below
+  #get '/users/:id', to: 'users#show'
 end
